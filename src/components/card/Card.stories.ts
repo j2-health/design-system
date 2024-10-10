d156bdd8048bdd5e625912bff672a9a0f4ec2306
Kyle Holzinger <kyleholzinger@hey.com>
2024-10-10 16:07:43 -0400

    feat(Card): add card to design system

---
+++ src/components/card/Card.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Card } from './Card'
+
+const meta = {
+  title: 'Components/Card',
+  component: Card,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Card>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    title: 'Card Title',
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante vel libero tincidunt, eget efficitur augue tincidunt. Nullam ac ante vel libero tincidunt, eget efficitur augue tincidunt.',
+  },
+}
