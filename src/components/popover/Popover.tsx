c434c848f6ba3e2e46e390001388619d9222cf33
Aaron Hong <aaron@j2health.com>
2024-10-22 12:13:43 -0400

    Bring in Popover styling

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -1,10 +1,14 @@
 import { Popover as AntdPopover, PopoverProps } from 'antd'
-
+import styles from './Popover.module.css'
 export type Props = Expand<PopoverProps>
 
 const Popover = ({ children, placement = 'top', ...props }: Props) => {
   return (
-    <AntdPopover {...props} placement={placement}>
+    <AntdPopover
+      overlayClassName={styles.j2Popover}
+      {...props}
+      placement={placement}
+    >
       {children}
     </AntdPopover>
   )
