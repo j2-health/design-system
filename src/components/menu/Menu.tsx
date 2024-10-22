1bd75523e83936259143bfe283234192d54e89fd
Aaron Hong <gnoha@users.noreply.github.com>
2024-10-22 16:37:32 -0400

    MKT 560 - Move AntDesign Theme to Design System (#16)
    * Add theme tokens and CSS variables to design system
    
    * Bring in Menu styling
    
    * Bring in Popover styling

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,10 @@
 import { Menu as AntdMenu, MenuProps as AntdMenuProps } from 'antd'
+import styles from './Menu.module.css'
 
 type Props = Expand<AntdMenuProps>
 
 const Menu = (props: Props) => {
-  return <AntdMenu {...props} />
+  return <AntdMenu className={styles.j2Menu} {...props} />
 }
 
 export { Menu }
