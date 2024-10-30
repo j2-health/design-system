076282ac575063238bd887fcf1770dcaa0644ab7
Aaron Hong <aaron@j2health.com>
2024-10-30 16:19:26 -0400

    Update alert icons with colors

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,9 +1,34 @@
+import * as React from 'react'
 import { Alert as AntdAlert, AlertProps } from 'antd'
-
+import {
+  CheckCircle,
+  Info,
+  WarningCircle,
+  XCircle,
+} from '@phosphor-icons/react'
 type Props = Expand<AlertProps>
 
 const Alert = (props: Props) => {
-  return <AntdAlert {...props} />
+  const icon = React.useMemo(() => {
+    switch (props.type) {
+      case 'success':
+        return (
+          <CheckCircle fill="var(--j2-color-success-text)" weight="duotone" />
+        )
+      case 'error':
+        return <XCircle fill="var(--j2-color-error-text)" weight="duotone" />
+      case 'info':
+        return <Info fill="var(--j2-color-info-text)" weight="duotone" />
+      case 'warning':
+        return (
+          <WarningCircle fill="var(--j2-color-warning-text)" weight="duotone" />
+        )
+      default:
+        return null
+    }
+  }, [props.type])
+
+  return <AntdAlert {...props} icon={icon} />
 }
 
 export { Alert }
