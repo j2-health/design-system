f57fd3bef37dec6d789b04d8778501208e8ec280
Gustavo Matias dos Santos <gumatias@gmail.com>
2024-11-13 15:22:42 -0600

    Adjust tickInterval type for better flexibility
    Updated the tickInterval prop type to allow for undefined or null values, enhancing flexibility in how the component can be used. This change accommodates scenarios where a tick interval may not be set, preventing potential errors and improving overall robustness.

--- src/components/barchart/BarChart.tsx
+++ src/components/barchart/BarChart.tsx
@@ -16,7 +16,7 @@ type BarChartProps = {
   series: Series[]
   min: number
   max: number | undefined | null
-  tickInterval: number
+  tickInterval: number | undefined | null
   tickFormat?: (value: number) => string
   xAxisTitle?: string
   yAxisTitle?: string
