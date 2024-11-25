8cef6b195ab4accd60b3a2ee9db0c21609a42858
Kyle Holzinger <kyleholzinger@hey.com>
2024-11-25 11:56:20 -0500

    chore(Collapse): remove extraneous redefinition of props

--- src/components/collapse/Collapse.tsx
+++ src/components/collapse/Collapse.tsx
@@ -1,25 +1,12 @@
 import { Collapse as AntdCollapse, CollapseProps } from 'antd'
 import { CaretDown } from '@phosphor-icons/react'
 
-export type Props = CollapseProps & {
-  /** Default active key */
-  defaultActiveKey?: string | string[]
-  /** Expand icon position */
-  expandIconPosition?: 'start' | 'end'
-}
+export type Props = Expand<CollapseProps>
 
-const Collapse = ({
-  defaultActiveKey,
-  expandIconPosition,
-  bordered,
-  ...props
-}: Props) => {
+const Collapse = ({ ...props }: Props) => {
   return (
     <AntdCollapse
       {...props}
-      defaultActiveKey={defaultActiveKey}
-      expandIconPosition={expandIconPosition}
-      bordered={bordered}
       expandIcon={({ isActive }) => (
         <CaretDown
           size={14}
