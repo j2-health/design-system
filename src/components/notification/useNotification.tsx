15f1ee4487333d2f6a6bc47d28b52e93e2580e48
Aaron Hong <gnoha@users.noreply.github.com>
2024-11-25 14:09:26 -0500

    MKT-538: Update useNotification to support NID notifications + style updates (#49)
    * Expose openNotification function type
    
    * Set notification styles
    
    * Use phosphor icons in notifications

--- src/components/notification/useNotification.tsx
+++ src/components/notification/useNotification.tsx
@@ -1,16 +1,60 @@
 import { notification } from 'antd'
 import type { NotificationArgsProps } from 'antd'
+import './Notification.css'
+import {
+  CheckCircle,
+  Info,
+  WarningCircle,
+  X,
+  XCircle,
+} from '@phosphor-icons/react'
 
 type NotificationType = 'success' | 'info' | 'warning' | 'error'
+
+export type OpenNotificationFunc = (
+  type: Expand<NotificationType>,
+  config: Expand<NotificationArgsProps>
+) => void
+
+const getIcon = (type: NotificationType) => {
+  switch (type) {
+    case 'error':
+      return (
+        <XCircle color="var(--j2-color-error)" weight="duotone" size={24} />
+      )
+    case 'success':
+      return (
+        <CheckCircle
+          color="var(--j2-color-success)"
+          weight="duotone"
+          size={24}
+        />
+      )
+    case 'info':
+      return <Info color="var(--j2-color-info)" weight="duotone" size={24} />
+    case 'warning':
+      return (
+        <WarningCircle
+          color="var(--j2-color-warning)"
+          weight="duotone"
+          size={24}
+        />
+      )
+  }
+}
 
 export const useNotification = () => {
   const [api, contextHolder] = notification.useNotification()
 
-  const openNotification = (
+  const openNotification: OpenNotificationFunc = (
     type: Expand<NotificationType>,
     config: Expand<NotificationArgsProps>
   ) => {
-    api[type](config)
+    api[type]({
+      icon: getIcon(type),
+      ...config,
+      closeIcon: <X color="var(--j2-color-icon)" size={22} />,
+    })
   }
 
   return { openNotification, contextHolder }
