4065eb5be096075ef59a7b179868d623f7ea265c
Aaron Hong <aaron@j2health.com>
2024-12-04 15:44:12 -0500

    Add ability to use preconfigured tags

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -1,7 +1,58 @@
+import * as React from 'react'
 import { Tag as AntdTag, TagProps } from 'antd'
+import {
+  ArrowsClockwise,
+  CheckCircle,
+  Gear,
+  Icon,
+  WarningCircle,
+  XCircle,
+} from '@phosphor-icons/react'
+import './Tag.css'
 
-type Props = Expand<TagProps>
+type Props = Expand<Omit<TagProps, 'icon'>> & {
+  status: 'default' | 'error' | 'success' | 'warning' | 'processing'
+  icon?: Icon
+  showIcon?: boolean
+}
 
-export const Tag = (props: Props) => {
-  return <AntdTag {...props} />
+const colorToIcon = {
+  error: XCircle,
+  success: CheckCircle,
+  warning: WarningCircle,
+  processing: ArrowsClockwise,
+  default: Gear,
+}
+
+const statusToColor = {
+  error: 'var(--j2-color-error-text)',
+  success: 'var(--j2-color-success)',
+  warning: 'var(--j2-color-warning-text)',
+  processing: 'var(--j2-color-primary)',
+  default: 'var(--j2-color-text)',
+}
+
+export const Tag = ({ status, showIcon = false, icon, ...props }: Props) => {
+  const iconComponent = React.useMemo(() => {
+    if (!showIcon) return null
+
+    const baseProps = {
+      className: 'j2-tag-icon',
+      size: 12,
+    }
+
+    const iconName: Icon = icon ?? colorToIcon[status]
+    const color: string = statusToColor[status]
+
+    return React.createElement(iconName, { color, ...baseProps })
+  }, [status, showIcon, icon])
+
+  return (
+    <AntdTag
+      {...props}
+      color={status}
+      icon={iconComponent}
+      className="j2-tag"
+    />
+  )
 }
