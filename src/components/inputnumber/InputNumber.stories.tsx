a484bd028c68164b21b6c04a73325522e377caab
Aaron Hong <aaron@j2health.com>
2025-01-08 14:25:18 -0500

    Fix InputNumber story to take into account Formik dependency

--- src/components/inputnumber/InputNumber.stories.tsx
+++ src/components/inputnumber/InputNumber.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { InputNumber } from './InputNumber'
+import { Form } from 'antd'
+import { Formik } from 'formik'
 
 const meta: Meta<typeof InputNumber> = {
   title: 'Components/InputNumber',
@@ -14,6 +16,17 @@ const meta: Meta<typeof InputNumber> = {
     },
     onChange: { action: 'changed' },
   },
+  decorators: [
+    (Story) => {
+      return (
+        <Formik initialValues={{ my_number: undefined }} onSubmit={() => {}}>
+          <Form>
+            <Story />
+          </Form>
+        </Formik>
+      )
+    },
+  ],
 }
 
 export default meta
@@ -21,6 +34,7 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
+    name: 'my_number',
     min: 0,
     max: 100,
     step: 1,
