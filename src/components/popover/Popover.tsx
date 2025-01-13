300c57712b16a862778c4874981ac6097e9ec6b8
Kyle Holzinger <kyleholzinger@hey.com>
2025-01-13 13:27:54 -0500

    feat(Popover): add popover header component (#75)

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -1,17 +1,53 @@
-import { Popover as AntdPopover, PopoverProps } from 'antd'
+import { Popover as AntdPopover, PopoverProps as AntdPopoverProps } from 'antd'
+import cx from 'classnames'
 import styles from './Popover.module.css'
-export type Props = Expand<PopoverProps>
 
-const Popover = ({ children, placement = 'top', ...props }: Props) => {
+type _Props = AntdPopoverProps & {
+  contentPadding?: boolean
+}
+
+export type PopoverProps = Expand<_Props>
+
+const Popover = ({
+  children,
+  placement = 'top',
+  contentPadding = true,
+  content,
+  ...props
+}: PopoverProps) => {
   return (
     <AntdPopover
       overlayClassName={styles.j2Popover}
       {...props}
       placement={placement}
+      content={() => {
+        if (!contentPadding) {
+          return (
+            <div className="-m-6">
+              {typeof content == 'function' ? content() : content}
+            </div>
+          )
+        }
+
+        return typeof content == 'function' ? content() : content
+      }}
     >
       {children}
     </AntdPopover>
   )
 }
 
-export { Popover }
+const PopoverHeader = ({ title }: { title: string }) => {
+  return (
+    <div
+      className={cx(
+        'py-2 px-3 items-center flex justify-between',
+        styles.popoverHeader
+      )}
+    >
+      {title}
+    </div>
+  )
+}
+
+export { Popover, PopoverHeader }
