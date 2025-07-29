0bdb60baf60c7ade0daeeed03141e5b727c9bd1b
Faith Komlo <faith@j2health.com>
2025-07-29 16:34:14 -0400

    J2NM-3311: 3e - Card Component (#111)
    * update border for card component + some design feedback
    
    * remove some redundant prop declaration
    
    * update snapshots
    
    * update card component spacing
    
    * update snapshots again

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -4,29 +4,22 @@ import { XIcon } from '@phosphor-icons/react'
 import './Drawer.css'
 
 type AllProps = DrawerProps & {
-  placement?: 'top' | 'bottom' | 'left' | 'right'
   fillPage?: boolean
   footer?: React.ReactNode
 }
 
 export type Props = Expand<AllProps>
 
-export const Drawer = ({
-  placement = 'bottom',
-  fillPage = false,
-  footer,
-  ...props
-}: Props) => {
+export const Drawer = ({ fillPage = false, footer, ...props }: Props) => {
   return (
     <AntdDrawer
       {...props}
       closable={false}
-      placement={placement}
       {...(fillPage && { height: '100vh', width: '100vw' })}
       {...(footer && { footer })}
       extra={
         <Space>
-          <Button type="link" onClick={props.onClose}>
+          <Button className="p-0" type="link" onClick={props.onClose}>
             <XIcon size={22} weight="regular" />
           </Button>
         </Space>
