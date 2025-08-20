09f998be6a55518736d3584ceed56298bee68ec9
Faith Komlo <faith@j2health.com>
2025-08-20 12:45:36 -0400

    give size default value

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,20 +4,29 @@ import { Card as AntdCard, CardProps } from 'antd'
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import './Card.css'
 
-export type Props = CardProps & {
+export type Props = Omit<CardProps, 'size'> & {
   /** Card title */
   title?: string
   /** Card contents */
   children: React.ReactNode
   /** Loading state */
   loading?: boolean
+  /** Card size */
+  size?: 'default' | 'small' | 'large'
 }
 
-const Card = ({ title, children, loading, ...props }: Props) => {
+const Card = ({
+  title,
+  children,
+  loading,
+  size = 'default',
+  ...props
+}: Props) => {
   return (
     <AntdCard
-      className={`j2-card j2-card-${props.size}`}
+      className={`j2-card j2-card-${size}`}
       {...props}
+      size={size === 'large' ? 'default' : size}
       title={title}
       loading={loading}
     >
