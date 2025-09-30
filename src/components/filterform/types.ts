c4e75c54fe5488bd277c257b896cbb4ee4c25b96
Guilherme Gonçalves <51221563+gagguilhermearaujo@users.noreply.github.com>
2025-09-30 09:57:52 -0300

    adding category_name to filterform

--- src/components/filterform/types.ts
+++ src/components/filterform/types.ts
@@ -49,6 +49,7 @@ type SelectFilter = {
     | Extract<Operator, 'equals' | 'notEqual' | 'blank' | 'notBlank'>
     | undefined
   values: string[]
+  category_name?: string
 }
 
 type NumberFilter = {
@@ -63,6 +64,7 @@ type NumberFilter = {
     | 'inRange'
     | undefined
   values: number[]
+  category_name?: string
 }
 
 type TextFilter = {
@@ -80,6 +82,7 @@ type TextFilter = {
       >
     | undefined
   values: string[]
+  category_name?: string
 }
 
 export type Filter = SelectFilter | NumberFilter | TextFilter
